9f0d263b7805c265a5f90f97b5c50c33715260f8
Nikita Sviridenko <nikita.sviridenko@voypost.com>
2020-05-09 12:46:21 +0200

    Add expo-av

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Video, Audio } from 'expo-av';
+
+const videos = {
+  VIMEO: 'https://player.vimeo.com/play/1783259589?s=414482682_1589063018_5c43a6595ca7de02335cce251299e962&loc=external&context=Vimeo%5CController%5CApi%5CResources%5CUser%5CVideoController.&download=1&filename=Untitled139.mp4',
+};
 
 export default function App() {
+  return <Video shouldPlay style={{ width: 200, height: 200 }} source={{ uri: videos.VIMEO }} useNativeControls />;
   return (
     <View style={styles.container}>
+      <Video shouldPlay style={{ width: 200, height: 200 }} source={{ uri: videos.VIMEO }} useNativeControls />
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
